8726c4345c58483dc3608246e4b6b5a64a75f011
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-02-21 18:51:05 -0300

    first configs - webpack, babel, json-server and vercel

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const jsonServer = require('json-server');
+const path = require('path');
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, '../server.json')); 
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1'
+}));
+server.use(router);
+
+module.exports = server;
---
+++ src/main.js
@@ -0,0 +1,5 @@
+"use strict"
+
+import "./styles/global.css"
+import "./styles/index.css"
+import "./styles/schedule.css"
