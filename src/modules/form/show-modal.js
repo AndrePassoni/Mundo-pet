7035e3471c29ac655c27522db476eb5ebf4f0da2
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-02-23 15:57:12 -0300

    showing modal when clicking new appointment button

---
+++ src/modules/form/show-modal.js
@@ -0,0 +1,6 @@
+const showModal = document.querySelector(".modal")
+const btnShow = document.querySelector(".btn-new")
+
+btnShow.addEventListener("click", function () {
+    showModal.classList.remove("display")
+})
