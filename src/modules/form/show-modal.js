e7f96d1be91084231f1ef29513389109052cbb8c
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-02-24 16:06:43 -0300

    adding function to close the modal

--- src/modules/form/show-modal.js
+++ src/modules/form/show-modal.js
@@ -3,8 +3,33 @@ const main = document.querySelector("main")
 const btnShow = document.querySelector(".btn-new")
 const filter = document.querySelector(".filter")
 
+// Função para mostrar o modal ao clicar no botão de novo agendamento
 btnShow.addEventListener("click", function () {
     modal.classList.remove("display")
     main.classList.add("filter")
     btnShow.classList.add("filter")
+})
+
+// Função para fechar o modal
+function closeModal() {
+    modal.classList.add("display")
+    main.classList.remove("filter")
+    btnShow.classList.remove("filter")
+}
+
+document.addEventListener("click", (event) => {
+
+    // Verifica se o modal está aberto
+    const isModalOpen = !modal.classList.contains("display")
+
+    // Verifica se o clique foi fora do modal
+    const isClickOutsideModal = !modal.contains(event.target)
+
+    // Verifica se o clique foi no botão de novo agendamento
+    const isClickOutsideButton = !btnShow.contains(event.target)
+
+    // Se todas as condições forem atendidas executa a função de fechar o modal
+    if (isModalOpen && isClickOutsideModal && isClickOutsideButton) {
+        closeModal()
+    }
 })
